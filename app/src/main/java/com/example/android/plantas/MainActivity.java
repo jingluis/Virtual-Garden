c7e23cae8ca17c48d3192b819963c89717a14dc2
jingluis <jing.luis.cao@est.fib.upc.edu>
2018-11-25 10:22:45 +0100

    imagenes bien

--- app/src/main/java/com/example/android/plantas/MainActivity.java
+++ app/src/main/java/com/example/android/plantas/MainActivity.java
@@ -62,18 +62,22 @@ public class MainActivity extends AppCompatActivity {
                     String s3 = String.valueOf(imag3.getTag());
                     if(s.equals("cactus1")){
                         imag.setImageResource(R.drawable.cactus4);
+                        imag.setTag("cactus4");
                         found = 1;
                     }
                     if(s1.equals("cactus1")){
                         imag1.setImageResource(R.drawable.cactus4);
+                        imag1.setTag("cactus4");
                         found = 1;
                     }
                     if(s2.equals("cactus1")){
                         imag2.setImageResource(R.drawable.cactus4);
+                        imag2.setTag("cactus4");
                         found = 1;
                     }
                     if(s3.equals("cactus1")){
                         imag3.setImageResource(R.drawable.cactus4);
+                        imag3.setTag("cactus4");
                         found = 1;
                     }
                 }
@@ -82,28 +86,40 @@ public class MainActivity extends AppCompatActivity {
                         ImageView imag = (ImageView) findViewById(R.id.primer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res")) imag.setTag("cactus1");
-                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.cactus4);
+                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.cactus4);
+                            imag.setTag("cactus4");
+                        }
                         else imag.setImageResource(R.drawable.cactus1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 1){
                         ImageView imag = (ImageView) findViewById(R.id.segon);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res1")) imag.setTag("cactus1");
-                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.cactus4);
+                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.cactus4);
+                            imag.setTag("cactus4");
+                        }
                         else imag.setImageResource(R.drawable.cactus1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 2){
                         ImageView imag = (ImageView) findViewById(R.id.tercer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res2")) imag.setTag("cactus1");
-                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.cactus4);
+                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.cactus4);
+                            imag.setTag("cactus4");
+                        }
                         else imag.setImageResource(R.drawable.cactus1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else{
                         ImageView imag = (ImageView) findViewById(R.id.quart);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res3")) imag.setTag("cactus1");
-                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.cactus4);
+                        if(s.equals("cactus1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.cactus4);
+                            imag.setTag("cactus4");
+                        }
                         else imag.setImageResource(R.drawable.cactus1);
                         imag.setVisibility(ImageView.VISIBLE);
                     }
@@ -126,19 +142,23 @@ public class MainActivity extends AppCompatActivity {
                     String s3 = String.valueOf(imag3.getTag());
                     if(s.equals("aloe1")){
                         imag.setImageResource(R.drawable.aloe4);
+                        imag.setTag("aloe4");
                         found = 1;
                     }
                     if(s1.equals("aloe1")){
                         imag1.setImageResource(R.drawable.aloe4);
+                        imag1.setTag("aloe4");
                         found = 1;
                     }
                     if(s2.equals("aloe1")){
                         imag2.setImageResource(R.drawable.aloe4);
-                        found  = 1;
+                        imag2.setTag("aloe4");
+                        found = 1;
                     }
                     if(s3.equals("aloe1")){
                         imag3.setImageResource(R.drawable.aloe4);
-                        found  = 1;
+                        imag3.setTag("aloe4");
+                        found = 1;
                     }
                 }
                 if(found == 0){
@@ -146,29 +166,42 @@ public class MainActivity extends AppCompatActivity {
                         ImageView imag = (ImageView) findViewById(R.id.primer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res")) imag.setTag("aloe1");
-                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.aloe4);
+                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.aloe4);
+                            imag.setTag("aloe4");
+                        }
                         else imag.setImageResource(R.drawable.aloe1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 1){
                         ImageView imag = (ImageView) findViewById(R.id.segon);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res1")) imag.setTag("aloe1");
-                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.aloe4);
+                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.aloe4);
+                            imag.setTag("aloe4");
+                        }
                         else imag.setImageResource(R.drawable.aloe1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 2){
                         ImageView imag = (ImageView) findViewById(R.id.tercer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res2")) imag.setTag("aloe1");
-                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.aloe4);
+                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.aloe4);
+                            imag.setTag("aloe4");
+                        }
                         else imag.setImageResource(R.drawable.aloe1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else{
                         ImageView imag = (ImageView) findViewById(R.id.quart);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res3")) imag.setTag("aloe1");
-                        if(s.equals("aloe1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.aloe4);
-                        else imag.setImageResource(R.drawable.aloe1);
+                        if (s.equals("aloe1") && imag.getVisibility() == imag.VISIBLE) {
+                            imag.setImageResource(R.drawable.aloe4);
+                            imag.setTag("aloe4");
+                        } else {
+                            imag.setImageResource(R.drawable.aloe1);
+                        }
                         imag.setVisibility(ImageView.VISIBLE);
                     }
                     setCount(count + 1);
@@ -191,18 +224,22 @@ public class MainActivity extends AppCompatActivity {
                     String s3 = String.valueOf(imag3.getTag());
                     if(s.equals("daisy1")){
                         imag.setImageResource(R.drawable.daisy4);
+                        imag.setTag("daisy4");
                         found = 1;
                     }
                     if(s1.equals("daisy1")){
                         imag1.setImageResource(R.drawable.daisy4);
+                        imag1.setTag("daisy4");
                         found = 1;
                     }
                     if(s2.equals("daisy1")){
                         imag2.setImageResource(R.drawable.daisy4);
+                        imag2.setTag("daisy4");
                         found = 1;
                     }
                     if(s3.equals("daisy1")){
                         imag3.setImageResource(R.drawable.daisy4);
+                        imag3.setTag("daisy4");
                         found = 1;
                     }
                 }
@@ -211,28 +248,40 @@ public class MainActivity extends AppCompatActivity {
                         ImageView imag = (ImageView) findViewById(R.id.primer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res")) imag.setTag("daisy1");
-                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.daisy4);
+                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.daisy4);
+                            imag.setTag("daisy4");
+                        }
                         else imag.setImageResource(R.drawable.daisy1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 1){
                         ImageView imag = (ImageView) findViewById(R.id.segon);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res1")) imag.setTag("daisy1");
-                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.daisy4);
+                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.daisy4);
+                            imag.setTag("daisy4");
+                        }
                         else imag.setImageResource(R.drawable.daisy1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 2){
                         ImageView imag = (ImageView) findViewById(R.id.tercer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res2")) imag.setTag("daisy1");
-                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.daisy4);
+                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.daisy4);
+                            imag.setTag("daisy4");
+                        }
                         else imag.setImageResource(R.drawable.daisy1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else{
                         ImageView imag = (ImageView) findViewById(R.id.quart);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res3")) imag.setTag("daisy1");
-                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.daisy4);
+                        if(s.equals("daisy1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.daisy4);
+                            imag.setTag("daisy4");
+                        }
                         else imag.setImageResource(R.drawable.daisy1);
                         imag.setVisibility(ImageView.VISIBLE);
                     }
@@ -257,19 +306,22 @@ public class MainActivity extends AppCompatActivity {
                     String s3 = String.valueOf(imag3.getTag());
                     if(s.equals("mint1")){
                         imag.setImageResource(R.drawable.mint4);
+                        imag.setTag("mint4");
                         found = 1;
-
                     }
                     if(s1.equals("mint1")){
                         imag1.setImageResource(R.drawable.mint4);
+                        imag1.setTag("mint4");
                         found = 1;
                     }
                     if(s2.equals("mint1")){
                         imag2.setImageResource(R.drawable.mint4);
+                        imag2.setTag("mint4");
                         found = 1;
                     }
                     if(s3.equals("mint1")){
                         imag3.setImageResource(R.drawable.mint4);
+                        imag3.setTag("mint4");
                         found = 1;
                     }
                 }
@@ -278,28 +330,40 @@ public class MainActivity extends AppCompatActivity {
                         ImageView imag = (ImageView) findViewById(R.id.primer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res")) imag.setTag("mint1");
-                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.mint4);
+                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.mint4);
+                            imag.setTag("mint4");
+                        }
                         else imag.setImageResource(R.drawable.mint1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 1){
                         ImageView imag = (ImageView) findViewById(R.id.segon);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res1")) imag.setTag("mint1");
-                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.mint4);
+                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.mint4);
+                            imag.setTag("mint4");
+                        }
                         else imag.setImageResource(R.drawable.mint1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else if(count % 4 == 2){
                         ImageView imag = (ImageView) findViewById(R.id.tercer);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res2")) imag.setTag("mint1");
-                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.mint4);
+                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.mint4);
+                            imag.setTag("mint4");
+                        }
                         else imag.setImageResource(R.drawable.mint1);
                         imag.setVisibility(ImageView.VISIBLE);
                     } else{
                         ImageView imag = (ImageView) findViewById(R.id.quart);
                         String s = String.valueOf(imag.getTag());
                         if(s.equals("res3")) imag.setTag("mint1");
-                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE)imag.setImageResource(R.drawable.mint4);
+                        if(s.equals("mint1") && imag.getVisibility()==imag.VISIBLE){
+                            imag.setImageResource(R.drawable.mint4);
+                            imag.setTag("mint4");
+                        }
                         else imag.setImageResource(R.drawable.mint1);
                         imag.setVisibility(ImageView.VISIBLE);
                     }
